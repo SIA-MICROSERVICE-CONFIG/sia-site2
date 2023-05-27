4c04ee7225095803d15b1892ca9f8ca25b926fca
katernate <krnternate@gmail.com>
2023-05-27 08:35:08 +0800

    Second Lab Activity Finals

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,84 +1,99 @@
 <?php
 
+
 namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\UserJob;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Models\User;
 use App\Traits\ApiResponser;
-use DB;
 
-Class UserController extends Controller {
+class UserController extends Controller
+{
     use ApiResponser;
-    
+
     private $request;
-    public function __construct(Request $request)
-    {
+
+    public $timestamps = false;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request){
         $this->request = $request;
     }
-    public function getUsers(){    
-
-        $users = DB::connection('mysql')    
-        ->select("Select * from tbluser");
-        return $this->successResponse($users);
+    
+    public function showUsers(){
+        $users = User::all();
+        return $this -> successResponse(User::all());
     }
 
-    public function index(){
+    public function showUser($id){
+        $user = User::findOrFail($id);
+        return $this->successResponse($user);
+    }
 
-        $users = User::all();
-        return $this->successResponse($users);
-    }   
-
-    public function add(Request $request){
-        
+    public function addUser(Request $request){
         $rules = [
             'username' => 'required|max:20',
             'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
+            // 'jobid' => 'required|numeric|min:1|not_in:0'
         ];
 
-        $this->validate($request,$rules);
+        $validate = $this->validate($request, $rules);
 
         $user = User::create($request->all());
+        // $userjob = UserJob::findOrFail($request->jobid);
+
         return $this->successResponse($user, Response::HTTP_CREATED);
-    }
-
-    public function show($id){
+    }   
         
-        $user = User::where('userid', $id)->first();
-        if($user){
-            return $this->successResponse($user);
-        }
-        {
-            return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
-        }
-    }
+    //  if ($validate){
+    //         $user = User::create($request->all());
 
-    public function update(Request $request, $id) { 
+    //         return $this->successResponse($user, 201);
+    //     }
+    //     else{
+    //         return $this->ErrorResponse("Operation Cannot be done.", 
+    //         Response::HTTP_UNPROCESSABLE_ENTITY);
+    //     }
+    // }
 
+    public function updateUser(Request $request, $id){
         $rules = [
             'username' => 'required|max:20',
-            'password' => 'required|max:20'
+            'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
+            // 'jobid' => 'required|numeric|min:1|not_in:0'
         ];
-    
-        $this->validate($request, $rules);
-    
-        $user = User::findOrFail($id);
-    
-        $user->fill($request->all());
-    
-        if ($user->isClean()) {
-            return Response()->json("At least one value must change", Response::HTTP_UNPROCESSABLE_ENTITY);
-        } else {
-            $user->save();
-            return $this->successResponse($user);
+
+        $validate = $this->validate($request, $rules);
+
+        // $userjob = UserJob::findOrFail($request->jobid);
+
+        if ($validate){
+
+            $user = User::findOrFail($id);
+            $user->fill($request->all());
+
+            if ($user->isClean()) {
+                return $this-> ErrorResponse("At least one value must change.", 
+                Response::HTTP_UNPROCESSABLE_ENTITY);
+            } else {
+                $user->save();
+                return $this->successResponse($user);
+            }
         }
     }
 
-    public function delete($id) { 
-
+    public function deleteUser($id){ 
         $user = User::findOrFail($id);
         $user->delete();
-    
+        
         return $this->successResponse($user);
     }
 }
